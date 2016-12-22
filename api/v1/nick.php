51c0563b586ee80dbc1e8a9d49736f217fd76eca
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2016-12-22 20:20:04 +0200

    Fix SQL so that we can get 'changed' and be more index friendly #4

--- api/v1/nick.php
+++ api/v1/nick.php
@@ -1,15 +1,24 @@
 <?php
 require_once(__DIR__.'/../../common.php');
 
+// Here are the individual SQL queries for each phase for clarity and
+// below it is a mash-up of them for effectiveness
+//
+// SELECT mac FROM visit WHERE ip=:ip ORDER BY leave DESC LIMIT 1;
+// SELECT id,changed FROM user_mac WHERE mac=:mac AND changed<:leave ORDER BY changed DESC LIMIT 1;
+// SELECT nick FROM user WHERE id=:id
+
 // Get person info by querying by latest IP
 $get_visitors = $db->prepare("
-	SELECT mac, hostname, nick
+	SELECT v.mac, hostname, nick, changed
 	FROM visit v
-	LEFT JOIN user u ON (SELECT id
-	                     FROM user_mac m
-	                     WHERE m.mac=v.mac AND m.changed<leave
-	                     ORDER BY m.changed DESC LIMIT 1
-	                    )=u.id
+	LEFT JOIN user_mac m ON (SELECT rowid
+	                         FROM user_mac
+	                         WHERE mac=v.mac AND changed<v.leave
+	                         ORDER BY changed DESC
+	                         LIMIT 1
+	                        )=m.rowid
+	LEFT JOIN user u ON m.id=u.id
 	WHERE ip=?
 	ORDER BY leave DESC
 	LIMIT 1
