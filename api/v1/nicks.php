a45a9740b7157d8a94e95de77c2c8fcbc66db313
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2016-12-22 22:11:00 +0200

    Implement nick list, /api/v1/nicks, closes #4

---
+++ api/v1/nicks.php
@@ -0,0 +1,15 @@
+<?php
+require_once(__DIR__.'/../../common.php');
+
+$get_nicks = $db->prepare("
+    SELECT nick FROM user ORDER by nick
+");
+
+$nicks = db_execute($get_nicks);
+$a = [];
+while (($data = $nicks->fetchArray(SQLITE3_NUM))) {
+    array_push($a, $data[0]);
+}
+
+header("Content-Type: application/json; charset=utf-8");
+print(json_encode($a)."\n");
