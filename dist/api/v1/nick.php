4b723e5806c5e77781450ffaaf345143d8bae858
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2017-01-04 03:56:20 +0200

    Do not allow CORS in the API for changing the nickname

--- dist/api/v1/nick.php
+++ dist/api/v1/nick.php
@@ -102,7 +102,4 @@ default:
 }
     
 header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Methods: GET, DELETE, PUT');
-header('Access-Control-Allow-Origin: *'); // FIXME TEMPORARY
-header('Vary: Origin');
 print(json_encode($o)."\n");
