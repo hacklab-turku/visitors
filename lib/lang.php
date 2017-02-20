3a0726ee68bbc2737ab0c98ab876b0cc804749af
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2017-02-20 19:17:42 +0200

    Move IRC reporting to parse_433 and use lang to define the language

--- lib/lang.php
+++ lib/lang.php
@@ -1,22 +1,43 @@
 <?php
-function leave_line($a) {
-    $msg = "Hacklab on nyt tyhjä. Paikalla oli";
-    $msg .= count($a) > 1 ? 'vat ' : ' ';
-    foreach ($a as $user => $visits) {
-        $msg .= "$user (";
-        foreach($visits as $visit) {
-            $msg .=
-                date('H:i', $visit['enter']).
-                '–'.
-                date('H:i', $visit['leave']).
-                ', ';
+class LangFinnishHacklab {
+    public static function leave_line($a) {
+        $msg = "Hacklab on nyt tyhjä. Paikalla oli";
+        $msg .= count($a) > 1 ? 'vat ' : ' ';
+        foreach ($a as $user => $visits) {
+            $msg .= "$user (";
+            foreach($visits as $visit) {
+                $msg .=
+                    date('H:i', $visit['enter']).
+                    '–'.
+                    date('H:i', $visit['leave']).
+                    ', ';
+            }
+            // Add closing brace before comma+space the hard way
+            $msg = substr($msg, 0, -2).'), ';
         }
-        // Add closing brace before comma+space the hard way
-        $msg = substr($msg, 0, -2).'), ';
+        return substr($msg, 0, -2); // Remove comma+space
     }
-    return substr($msg, 0, -2); // Remove comma+space
-}
 
-function join_line(&$a) {
-    return "Ensimmäisenä saapui ".$a['nick'];
+
+    public static function join_line($a) {
+        return "Ensimmäisenä saapui ".$a['nick'];
+    }
+
+    public static function evening_start($visits) {
+        if (count($visits) === 0) {
+            $msg = "Joku painoi \"kerhoilta alkaa\"-painiketta. Kuka olet?";
+        } else {
+            $msg = "Kerhoilta alkoi, paikalla ";
+            $msg .= count($visits) > 1 ? 'ovat ' : 'on ';
+            foreach ($visits as $user) {
+            $msg .=
+                $user['nick'].' (saapui '.
+                date('H:i', $user['enter']).
+                '), ';
+            }
+            $msg = substr($msg, 0, -2); // Remove comma+space
+            $msg .= ". Tervetuloa!";
+        }
+        return $msg;
+    }
 }
