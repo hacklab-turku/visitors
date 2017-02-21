30afc3af193cd68a20d0089d143f41f27fd44478
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2017-02-21 22:02:23 +0200

    Move button activities to lang.php (which is a bit misleading but practical)

--- lib/irc.php
+++ lib/irc.php
@@ -3,10 +3,17 @@
 // Connect to the message queue
 $irc_resource = fopen(getenv('HOME').'/ii/rajaniemi.freenode.net/in', 'w');
 
-function to_irc($msg) {
+function to_irc($msg, $chan) {
     // TODO make this not hardcoded
     global $irc_resource;
     // FIXME msg escaping of newline, reading of return values etc.
-    fwrite($irc_resource, "/notice #hacklab.jkl :$msg\n");
+    fwrite($irc_resource, "/notice $chan :$msg\n");
     fflush($irc_resource);
+}
+
+function lang_out($msg = NULL, $save_state = TRUE) {
+    return (object)[
+        'irc' => $msg,
+        'save_state' => $save_state,
+    ];
 }
--- lib/lang.php
+++ lib/lang.php
@@ -1,5 +1,7 @@
 <?php
 class LangFinnishHacklab {
+    public static $chan = '#hacklab.jkl';
+    
     public static function leave_line($a) {
         $msg = "Hacklab on nyt tyhjä. Paikalla oli";
         $msg .= count($a) > 1 ? 'vat ' : ' ';
@@ -39,5 +41,46 @@ class LangFinnishHacklab {
             $msg .= ". Tervetuloa!";
         }
         return $msg;
+    }
+
+    public static function button($e) {
+        switch ($e) {
+        case 'rtl_error':
+            return lang_out('Softaradio meni sekaisin. :-/ Voisiko joku ottaa sen mustan tikun irti reitittimestä ja laittaa takaisin?');
+        case 'rtl_ok':
+            return lang_out('Kiitos, softaradio toimii taas. <3');
+        default:
+            switch ($e->model) {
+            case 'Generic Remote':
+                // Skip if not our remote and if button is released.
+                if ($e->released || $e->chan !== 0) break;
+
+                // Now parsing the events for buttons
+                if ($e->button === 0 && $e->pressed) {
+                    exec('sudo systemctl start qra');
+                    return lang_out('Hacklabin valot syttyivät!');
+                } else if ($e->button === 0 && !$e->pressed) {
+                    exec('sudo systemctl stop qra');
+                    return lang_out('Hacklabin valot sammuivat!');
+                } else if ($e->button === 2 && $e->pressed) {
+                    return lang_out('Nyt on eeppistä settiä! :-O');
+                } else if ($e->button === 3 && $e->pressed) {
+                    global $dhcp_lease_secs;
+                    // Search current visitors
+                    $req = [
+                        'lease' => $dhcp_lease_secs,
+                        'now' => time(),
+                    ];
+                    return lang_out(self::evening_start(get_visitors($req)));
+                } else if ($e->button === 3 && !$e->pressed) {
+                    return lang_out("Labilta ollaan tekemässä lähtöä...");
+                }
+                break;
+            case 'Generic temperature sensor 1':
+                if ($e->id === 0 && $e->temperature_C == 0.000) {
+                    return lang_out('Ding! Dong!', FALSE);
+                }
+            }
+        }
     }
 }
