cf433fc557a2f9259d012e0b033c96b65088030b
Joel Lehtonen <joel.lehtonen@iki.fi>
2019-12-07 03:10:12 +0200

    Add HTML formatting to join and leave messages

--- lib/localization.php
+++ lib/localization.php
@@ -55,25 +55,47 @@ class LocalizationHacklabJkl {
 
     public function last_leave($a) {
         $msg = "Hacklab on nyt tyhjä. Paikalla oli";
-        $msg .= count($a) > 1 ? 'vat ' : ' ';
+        $msg .= count($a) > 1 ? 'vat' : '';
+
+        // Matrix HTML message
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->appendChild($dom->createTextNode($msg.":"));
+        $table = $dom->createElement("ul");
+        $msg .= " ";
+
         foreach ($a as $user => $visits) {
+            $row = $dom->createElement("li");
+            $row->appendChild($dom->createElement("strong",$user));
+
             $msg .= "$user (";
+            $range = "";
             foreach($visits as $visit) {
-                $msg .=
+                $range .=
                     date('H:i', $visit['enter']).
                     '–'.
                     date('H:i', $visit['leave']).
                     ', ';
             }
             // Add closing brace before comma+space the hard way
-            $msg = substr($msg, 0, -2).'), ';
+            $range = substr($range, 0, -2);
+
+            $msg .= $range . '), ';
+            $row->appendChild($dom->createTextNode(" ".$range));
+            $table->appendChild($row);
         }
-        $this->notice(substr($msg, 0, -2)); // Remove comma+space
+        $dom->appendChild($table);
+        $this->notice(substr($msg, 0, -2), $dom); // Remove comma+space
     }
 
-
     public function first_join($a) {
-        $this->notice("Ensimmäisenä saapui ".$a['nick']);
+        $msg = "Ensimmäisenä saapui ";
+
+        // Matrix HTML message
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->appendChild($dom->createTextNode($msg));
+        $dom->appendChild($dom->createElement("strong",$a['nick']));
+
+        $this->notice($msg.$a['nick'], $dom);
     }
 
     public function evening_start($visits) {
